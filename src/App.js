c2d3bb244b0e209b1191cf0ed09d508b6d7013f0
Aryan <aryanverma170372@gmail.com>
2022-08-07 11:21:55 +0530

    getting started with FeedbackForm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData); // app level state accessed in feedbacklist
@@ -16,6 +17,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         {/* feedback prop={feedback state} */}
         <FeedbackList feedback={feedback} handleDeleteforApp={deleteFeedback} />
