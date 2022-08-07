5965676b92c38f0cfbad986707b76fc36006b841
Aryan <aryanverma170372@gmail.com>
2022-08-07 21:34:33 +0530

    feat: addFeedback function implemented

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,10 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
   const [text, setText] = useState("");
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
@@ -22,9 +23,22 @@ function FeedbackForm() {
     }
     setText(e.target.value);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length >= 10) {
+      const newFeedback = {
+        text, // Shortcut to say text:text, rating:rating
+        rating,
+      };
+
+      handleAdd(newFeedback);
+      setText("");
+    }
+  };
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
         <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
