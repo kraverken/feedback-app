6e974b57f509fbacb4940765c8f4e87fc981cf4f
Aryan <aryanverma170372@gmail.com>
2022-08-07 11:00:45 +0530

    feat: FeedbackStats and deleteHandle called

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
-import { useState } from "react";
+import FeedbackStats from "./components/FeedbackStats";
+
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
+  const [feedback, setFeedback] = useState(FeedbackData); // app level state accessed in feedbacklist
+
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackStats feedback={feedback} />
+        {/* feedback prop={feedback state} */}
+        <FeedbackList feedback={feedback} handleDeleteforApp={deleteFeedback} />
       </div>
     </>
   );
