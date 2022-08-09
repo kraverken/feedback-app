4917aee4c320b24c4ae2d029b89cbb456ae36800
Aryan <aryanverma170372@gmail.com>
2022-08-09 16:05:17 +0530

    feat: update values in database(PUT)

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -45,9 +45,18 @@ export const FeedbackProvider = ({ children }) => {
     });
   };
 
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = await response.json();
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
   return (
