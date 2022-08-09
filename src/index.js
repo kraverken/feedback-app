24820ff93ba3c3f69bf0d510db65a5c16dddbf4b
Aryan <aryanverma170372@gmail.com>
2022-08-09 11:56:00 +0530

    updated to React18 render mode

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-// ReactDOM.render isnt supported in REACT 18
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(<App />);
