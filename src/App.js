6cdb44f30a16296fb765f7dcab5c56f0df38d9f4
Aryan <aryanverma170372@gmail.com>
2022-08-09 10:40:32 +0530

    feat: Started with Context and Provider

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
 import AboutIconLink from "./components/AboutIconLink";
+import { FeedbackProvider } from "./context/FeedbackContext";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData); // app level state accessed in feedbacklist
@@ -24,30 +25,32 @@ function App() {
   };
   return (
     <>
-      <Router>
-        <Header />
-        <div className="container">
-          <Routes>
-            <Route
-              exact
-              path="/"
-              element={
-                <>
-                  <FeedbackForm handleAdd={addFeedback} />
-                  <FeedbackStats feedback={feedback} />
-                  {/* feedback prop={feedback state} */}
-                  <FeedbackList
-                    feedback={feedback}
-                    handleDeleteforApp={deleteFeedback}
-                  />
-                </>
-              }
-            ></Route>
-            <Route path="/about" element={<AboutPage />} />
-          </Routes>
-        </div>
-        <AboutIconLink />
-      </Router>
+      <FeedbackProvider>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={
+                  <>
+                    <FeedbackForm handleAdd={addFeedback} />
+                    <FeedbackStats feedback={feedback} />
+                    {/* feedback prop={feedback state} */}
+                    <FeedbackList
+                      feedback={feedback}
+                      handleDeleteforApp={deleteFeedback}
+                    />
+                  </>
+                }
+              ></Route>
+              <Route path="/about" element={<AboutPage />} />
+            </Routes>
+          </div>
+          <AboutIconLink />
+        </Router>
+      </FeedbackProvider>
     </>
   );
 }
---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,22 @@
+import { useState, createContext } from "react";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState({
+    id: 1,
+    text: "This comes from the context",
+    rating: 10,
+  });
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
