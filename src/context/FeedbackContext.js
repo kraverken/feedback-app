07963286e8ace7fdf741549ef69dc779d95af392
Aryan <aryanverma170372@gmail.com>
2022-08-09 15:01:43 +0530

    getting data from db using async await fetch

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -4,9 +4,23 @@ import { v4 as uuidv4 } from "uuid";
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [feedback, setFeedback] = useState([]);
-
   const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []); // run only once on load
+
+  //   Fetch Feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    );
+    const data = await response.json();
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
@@ -34,11 +48,12 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        isLoading,
         feedbackEdit, //state
         deleteFeedback,
         addFeedback,
-        editFeedback, //function
         updateFeedback,
+        editFeedback, //function
       }}
     >
       {children}
